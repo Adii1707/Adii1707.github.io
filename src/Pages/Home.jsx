b8a5a24c431212dceeef1f3effc33993175d7fc8
Aditya Singh Ghosh <89689829+Adii1707@users.noreply.github.com>
2022-12-14 00:18:40 +0530

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -25,7 +25,7 @@ const Home = ()=>{
               download={true}
               smooth
               duration={500}
-              className="group mt-1 text-white w-fit px-6 py-3 my-2 flex items-center rounded-md 
+              className="group mt-3 text-white w-fit px-6 py-3 my-2 flex items-center rounded-md 
               bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer"
             >
               Resume
